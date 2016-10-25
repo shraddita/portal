d75b5aa3bd0f0d832c86c9a8acd442359a30d690
Ian Matthew Morey <ian.matthew.morey@gmail.com>
2016-10-25 13:58:57 -0400

    fixed bug -- layers now display

--- js/carto.js
+++ js/carto.js
@@ -1,14 +1,21 @@
 var map;
 
-map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 10)
+map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 10);
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: 'Positron'}).addTo(map);
 
 var allLayers = 'https://nycem.carto.com/u/imorey/api/v2/viz/217510a8-447a-11e6-81f4-0e05a8b3e3d7/viz.json';
 
+
 cartodb.createLayer(map, allLayers, {https: true})
 	.addTo(map)
-	//.on('done', function(layer) {
+	.on('done', function(layer) {
+		
+	}).on('error', function() {
+    //log the error
+    });
+  
+  //.on('done', function(layer) {
 		//censusTract = layer.getSubLayer(0);
 		//censusTract.setInteraction(false);
 
