155baec5d0fa3b1d34ea5da50cd51d73c84e0501
shraddita <sramani@OEM.NYCNET>
2016-10-25 14:53:41 -0400

    clean up scripts; increase initial zoom

--- js/carto.js
+++ js/carto.js
@@ -1,6 +1,6 @@
 var map;
 
-map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 10);
+map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 11);
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: 'Positron'}).addTo(map);
 
@@ -10,18 +10,14 @@ var allLayers = 'https://nycem.carto.com/u/imorey/api/v2/viz/217510a8-447a-11e6-
 cartodb.createLayer(map, allLayers, {https: true})
 	.addTo(map)
 	.on('done', function(layer) {
-		
-	}).on('error', function() {
-    //log the error
-    });
-  
-  //.on('done', function(layer) {
-		//censusTract = layer.getSubLayer(0);
+		censusTract = layer.getSubLayer(0);
 		//censusTract.setInteraction(false);
 
-		//nTabArea = layer.getSubLayer(1); 
+		nTabArea = layer.getSubLayer(1); 
 		//nTabArea.hide();  //hide neighborhood polygons
 		//nTabArea.on('featureClick', processNeighborhood);
-	//}).on('error', function() {
-		//log the error
-	//});
+	})
+	.on('error', function() {
+    //log the error
+	alert("some error occurred: " + err);
+    });
