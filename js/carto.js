c5116560cb41c06c0f91804025ca99136adbc1cf
shraddita <sramani@OEM.NYCNET>
2016-10-25 16:27:19 -0400

    add cartocss to fill NTA area on hover

--- js/carto.js
+++ js/carto.js
@@ -6,6 +6,23 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attri
 
 var allLayers = 'https://nycem.carto.com/u/imorey/api/v2/viz/217510a8-447a-11e6-81f4-0e05a8b3e3d7/viz.json';
 
+function geometryHover(layer) {
+
+    function featureOver(e, pos, latlng, data) {
+        var cartocsshover = "#cscl_neighborhood_20141110{ polygon-fill: #3E7BB6; polygon-opacity: 0; line-color: #5CA2D1; line-width: 1; line-opacity: 1;}" + "#cscl_neighborhood_20141110 [ cartodb_id = "+ data.cartodb_id +"] {polygon-opacity: 0.5;}"
+        
+		layer.setCartoCSS(cartocsshover);
+    }
+
+    function featureOut() {
+        layer.setCartoCSS("#cscl_neighborhood_20141110{ polygon-fill: #3E7BB6; polygon-opacity: 0; line-color: #5CA2D1; line-width: 1; line-opacity: 1;}");
+    }
+
+    layer.on('featureOver', featureOver);
+    layer.on('featureOut', featureOut);
+    layer.setInteraction(true);
+
+}
 
 cartodb.createLayer(map, allLayers, {https: true})
 	.addTo(map)
@@ -16,6 +33,8 @@ cartodb.createLayer(map, allLayers, {https: true})
 		nTabArea = layer.getSubLayer(1); 
 		//nTabArea.hide();  //hide neighborhood polygons
 		//nTabArea.on('featureClick', processNeighborhood);
+		
+		geometryHover(nTabArea);
 	})
 	.on('error', function() {
     //log the error
