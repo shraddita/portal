6403d4f25fbd14eabe54e853e730fe447818f678
Jean Behrend <jeanbehrend@gmail.com>
2016-10-27 15:48:11 -0400

    added an "s"

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 
 map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 11);
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: 'Positron'}).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: 'Positron'}).addTo(map);
 
 var allLayers = 'https://nycem.carto.com/u/imorey/api/v2/viz/217510a8-447a-11e6-81f4-0e05a8b3e3d7/viz.json';
 
